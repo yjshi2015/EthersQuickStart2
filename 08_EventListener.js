b49c8bfa7948fa5904df1ed58f89101d45460165
yjshi <495054021@qq.com>
2023-08-20 19:32:34 +0800

    add event listener

---
+++ 08_EventListener.js
@@ -0,0 +1,35 @@
+/**
+ * 监听event事件
+ * contract.once("eventName", callback):监听一次
+ * contract.on("eventName", callback):持续监听
+ * 
+ * todo
+ * 1、ethers官方文档地址：https://docs.ethers.org/v6/api/contract/#BaseContract
+ * 2、DELETE 主网KEY
+ */
+
+const ethers = require("ethers");
+
+const ALCHEMY_SEPOLIA_KEY = 'y0RKCxxxxxxxxxxxEQOZcqH';
+const provider = new ethers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`);
+
+const abiUSDT = [
+    "event Transfer(address indexed from, address indexed to, uint value)"
+];
+const addressUSDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+//生成合约对象
+const contractUSDT = new ethers.Contract(addressUSDT, abiUSDT, provider);
+
+const main = async () => {
+    console.log('\n1.利用contract.once(),监听一次Transfer事件');
+    contractUSDT.once('Transfer', (from, to, value) => {
+        console.log(`${from} -> ${to} ${ethers.formatUnits(ethers.getBigInt(value), 6)}`);
+    });    
+    
+    console.log('\n2.利用contract.on(),持续监听Transfer事件');
+    contractUSDT.on('Transfer', (from, to, value) => {
+        console.log(`${from} -> ${to} ${ethers.formatUnits(ethers.getBigInt(value), 6)}`);
+    });
+}
+
+main()
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # EthersQuickStart2
  
 ## todo list
-- 05demo中的WETH内容，参见WTH中的solidity讲解
-- 06demo中的ERC20内容
+- ethers课程(doing)
+ + 05demo中的WETH内容，参见WTH中的solidity讲解
+ + 06demo中的ERC20内容
+- JavaScript课程
+- react课程-极客时间(先看git上的项目，因为dapp不是纯react，它是react和ethers的混合体，所以实战更有用)
+- 以太坊、比特币白皮书
+
+
+
