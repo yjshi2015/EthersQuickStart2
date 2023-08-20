9fe651c42b8ac7e908e2a572d9d686dd430c6fec
yjshi <495054021@qq.com>
2023-08-20 15:52:27 +0800

    deploy contract

--- 05_WriteContract.js
+++ 05_WriteContract.js
@@ -1,12 +1,19 @@
 /**
  * 对合约进行写操作
  * 查看ethers官网对应的API
+ * 
+ * todo syj
+ * 中午咨询下如何在goerlia网上领取测试币？？？
+ * 曲线救国的方式：把goerlia的合约部署到sepolia测试网上
+ * 
+ * Note: 
+ * abi一定不要手写，因为不会提示语法错误，public写成了pubic，导致运行失败
  */
 const ethers = require('ethers');
 
 //一、生成provider
-const ALCHEMY_KEY = 'dTKTdrFoicAZBCIROLne9B8ranAu7foA';
-const provider = new ethers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`);
+const ALCHEMY_GOERLI_KEY = 'dTKTdrFoicAZBCIROLne9B8ranAu7foA';
+const provider = new ethers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_GOERLI_KEY}`);
 
 //二、生成wallet
 const privateKey = '5f5e109d57e793f4886f4b406b0e013e92c76b0273d3278e8a956af7d877dff0';
@@ -17,7 +24,7 @@ const abiWETH = [
     "function balanceOf(address) public view returns(uint)",
     "function deposit() public payable",
     "function transfer(address, uint) public returns (bool)",
-    "function withdraw(uint) pubic",
+    "function withdraw(uint) public",
 ];
 
 const addressWETH = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'; //goerli测试网地址
@@ -29,12 +36,17 @@ const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet);
 
 const main = async () => {
     console.log(`begin-------------------`);
-/**
+
     //调用只读函数
     const address = await wallet.getAddress();
-    const balance = await contractWETH.balanceOf(address);
-    console.log(`1.钱包账户 ${address} 当前的余额为${ethers.formatEther(balance)}`);
+    const balanceWETH = await contractWETH.balanceOf(address);
+    console.log(`1.钱包账户 ${address} 当前的WETH余额为${ethers.formatEther(balanceWETH)}`);
 
+    //查询的是钱包主账户的余额
+    const balanceETH = await provider.getBalance(wallet);
+    console.log(`2.钱包 ${wallet} 当前的ETH余额为${ethers.formatEther(balanceETH)}`);
+
+/**
     //调用写函数deposit，写入0.001 ETH
     const tx = await contractWETH.deposit({value : ethers.parseEther("0.001")});
     await tx.wait();//等待交易上链
---
+++ 06_DeployWETHContract.js
@@ -0,0 +1,89 @@
+/**
+ * 利用ethers部署合约，将05中的WETH合约发布到sepolia网络中，再进行deposit和transfer操作
+ */
+
+const ethers = require('ethers');
+//链接节点服务商
+const ALCHEMY_SEPOLIA_KEY = '2vsw2JgOi6Hq-6Ky9RvvKnL4f88kg5qZ';
+const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`);
+
+//生成钱包signer
+const privateKey = '5f5e109d57e793f4886f4b406b0e013e92c76b0273d3278e8a956af7d877dff0';
+const wallet = new ethers.Wallet(privateKey, provider);
+
+//在Remix中生成abi和bytecode
+const abiWETH = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}];
+//或者手写abi
+const abiERC20_1 = [
+    "function balanceOf(address) public view returns(uint)",
+    "function deposit() public payable",
+    "function transfer(address, uint) public returns (bool)",
+    "function withdraw(uint) public",
+];
+
+const bytecodeWETH = '606060405260408051908101604052600d81527f57726170706564204574686572000000000000000000000000000000000000006020820152600090805161004b9291602001906100b1565b5060408051908101604052600481527f5745544800000000000000000000000000000000000000000000000000000000602082015260019080516100939291602001906100b1565b506002805460ff1916601217905534156100ac57600080fd5b61014c565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100f257805160ff191683800117855561011f565b8280016001018555821561011f579182015b8281111561011f578251825591602001919060010190610104565b5061012b92915061012f565b5090565b61014991905b8082111561012b5760008155600101610135565b90565b6106c28061015b6000396000f3006060604052600436106100ae5763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166306fdde0381146100b8578063095ea7b31461014257806318160ddd1461017857806323b872dd1461019d5780632e1a7d4d146101c5578063313ce567146101db57806370a082311461020457806395d89b4114610223578063a9059cbb14610236578063d0e30db0146100ae578063dd62ed3e14610258575b6100b661027d565b005b34156100c357600080fd5b6100cb6102d3565b60405160208082528190810183818151815260200191508051906020019080838360005b838110156101075780820151838201526020016100ef565b50505050905090810190601f1680156101345780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561014d57600080fd5b610164600160a060020a0360043516602435610371565b604051901515815260200160405180910390f35b341561018357600080fd5b61018b6103dd565b60405190815260200160405180910390f35b34156101a857600080fd5b610164600160a060020a03600435811690602435166044356103eb565b34156101d057600080fd5b6100b6600435610531565b34156101e657600080fd5b6101ee6105df565b60405160ff909116815260200160405180910390f35b341561020f57600080fd5b61018b600160a060020a03600435166105e8565b341561022e57600080fd5b6100cb6105fa565b341561024157600080fd5b610164600160a060020a0360043516602435610665565b341561026357600080fd5b61018b600160a060020a0360043581169060243516610679565b600160a060020a033316600081815260036020526040908190208054349081019091557fe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c915190815260200160405180910390a2565b60008054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103695780601f1061033e57610100808354040283529160200191610369565b820191906000526020600020905b81548152906001019060200180831161034c57829003601f168201915b505050505081565b600160a060020a03338116600081815260046020908152604080832094871680845294909152808220859055909291907f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b9259085905190815260200160405180910390a350600192915050565b600160a060020a0330163190565b600160a060020a0383166000908152600360205260408120548290101561041157600080fd5b33600160a060020a031684600160a060020a03161415801561045b5750600160a060020a038085166000908152600460209081526040808320339094168352929052205460001914155b156104c257600160a060020a03808516600090815260046020908152604080832033909416835292905220548290101561049457600080fd5b600160a060020a03808516600090815260046020908152604080832033909416835292905220805483900390555b600160a060020a038085166000818152600360205260408082208054879003905592861680825290839020805486019055917fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9085905190815260200160405180910390a35060019392505050565b600160a060020a0333166000908152600360205260409020548190101561055757600080fd5b600160a060020a033316600081815260036020526040908190208054849003905582156108fc0290839051600060405180830381858888f19350505050151561059f57600080fd5b33600160a060020a03167f7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b658260405190815260200160405180910390a250565b60025460ff1681565b60036020526000908152604090205481565b60018054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103695780601f1061033e57610100808354040283529160200191610369565b60006106723384846103eb565b9392505050565b6004602090815260009283526040808420909152908252902054815600a165627a7a723058207d2ba47f0307da0f1f4462eb8d07348ad197376ec16bc0818f88a5225004f6dd0029';
+
+//部署合约
+const factoryWETH = new ethers.ContractFactory(abiWETH, bytecodeWETH, wallet);
+
+const main = async () => {
+    /**
+    //填入构造函数参数
+    const contractWETH = await factoryWETH.deploy();
+    console.log(`1.部署合约`);
+    console.log(`合约地址${contractWETH.target}`);
+    console.log(`部署合约的交易详情`);
+    console.log(contractWETH.deploymentTransaction());
+    console.log(`等待合约部署上链`);
+    await contractWETH.waitForDeployment();
+    console.log(`合约已上链`);
+    console.log(`合约地址: ${await contractWETH.getAddress()}`);
+     */
+
+    // WETH合约地址（sepolia测试网）
+    const addressWETH = '0x2d0339D0244A0085C1d7398c16449a2d39c97E38';
+    // 声明可写合约
+    const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet);
+
+    const address = await wallet.getAddress()
+    // 1. 读取WETH合约的链上信息（WETH abi）
+    console.log("\n1. 读取WETH余额")
+    const balanceWETH = await contractWETH.balanceOf(address)
+    console.log(`存款前WETH持仓: ${ethers.formatEther(balanceWETH)}`)
+    //读取钱包内ETH余额
+    const balanceETH = await provider.getBalance(wallet)
+    console.log(`钱包ETH余额: ${ethers.formatEther(balanceETH)}`)
+
+    // 如果钱包ETH足够
+    if(ethers.formatEther(balanceETH) > 0.0015){
+
+        // 2. 调用desposit()函数，将0.001 ETH转为WETH
+        console.log("\n2. 调用desposit()函数，存入0.001 ETH")
+        // 发起交易
+        const tx = await contractWETH.deposit({value: ethers.parseEther("0.001")})
+        // 等待交易上链
+        await tx.wait()
+        console.log(`交易详情：`)
+        console.log(tx)
+        const balanceWETH_deposit = await contractWETH.balanceOf(address)
+        console.log(`存款后WETH持仓: ${ethers.formatEther(balanceWETH_deposit)}\n`)
+
+   
+        // 3. 调用transfer()函数，将0.001 WETH转账给 vitalik
+        console.log("\n3. 调用transfer()函数，给vitalik转账0.001 WETH")
+        // 发起交易
+        addressVitalik = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
+        const tx2 = await contractWETH.transfer(addressVitalik, ethers.parseEther("0.001"))
+        // 等待交易上链
+        await tx2.wait()
+        const balanceWETH_transfer = await contractWETH.balanceOf(address)
+        console.log(`转账后WETH持仓: ${ethers.formatEther(balanceWETH_transfer)}\n`)
+        
+    }else{
+        // 如果ETH不足
+        console.log("ETH不足，去水龙头领一些Goerli ETH")
+        console.log("1. chainlink水龙头: https://faucets.chain.link/goerli")
+        console.log("2. paradigm水龙头: https://faucet.paradigm.xyz/")
+    }
+}
+main()
