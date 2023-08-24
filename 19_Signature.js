3788caacfc9afd68fa0584f7fd875176ebeae2ae
yjshi <495054021@qq.com>
2023-08-24 23:48:22 +0800

    add 签名白名单

---
+++ 19_Signature.js
@@ -0,0 +1,90 @@
+/**
+ * 通过签名分发白名单流程
+ * 
+ * 1.指定签名的钱包，服务器保管singer钱包的公私钥对
+ * 2.对allowList白名单用户地址及tokenId，生成msgHash
+ * 3.用signer钱包给msgHash签名,存储于中心化的后端
+ * 4.部署NFT合约，并在初始化函数中写入签名的公钥
+ * 5.用户mint时，填入address和tokenId，想中心化后端获取该地址的签名结果
+ * 6.调用mint铸造
+ * 
+ * 相较于Merkle的优点：
+ * ①动态，支持白名单的灵活添加和删除
+ * ②更节省gas，Merkle中如果增加了白名单，需要改root，而这里不需要
+ * 
+ * todo syj
+ * 1.验证JSON格式合约部署码
+ * 2.合约发布失败，不管是Remix的bytecode还是JSON文件，哪怕demo拷贝过来，也是错误百出
+ */
+
+const {ethers} = require('ethers');
+const contractJson = require('./19_contract.json');
+
+console.log("\n1.创建provider和wallet");
+const ALCHEMY_SEPOLIA_KEY = '2vsw2JgOi6Hq-6Ky9RvvKnL4f88kg5qZ';
+const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`);
+
+const privateKey = '5f5e109d57e793f4886f4b406b0e013e92c76b0273d3278e8a956af7d877dff0';
+const wallet = new ethers.Wallet(privateKey, provider);
+console.log(`wallet地址: ${wallet.address}`);
+
+console.log("\n2.模拟中心化后端生成消息签名");
+//大壮
+const account = "0xd266Fb4a78bF6C93b6adb9493cd434A7dFA403E1";
+const tokenId = 0;
+//等效于solidity中keccak256(abi.encodePacked(account, tokenId))
+const msgHash = ethers.solidityPackedKeccak256(
+    ['address', 'uint256'],
+    [account, tokenId]
+)
+console.log(`msgHash: ${msgHash}`)
+
+const main = async () => {
+    //用项目方钱包(私钥)生成签名
+    const msgHashBytes = ethers.getBytes(msgHash);
+    const signature = await wallet.signMessage(msgHashBytes);
+    console.log(`签名:${signature}`)
+
+
+    console.log("\n3.利用合约工厂部署合约");
+    // const abiNFT = [
+    //     "constructor(address)",
+    //     "function name() public view returns (string)",
+    //     "function symbol() public view returns (string)",
+    //     "function mint(address, uint256, bytes) external",
+    //     "function ownerOf(uint256) public view returns (address)",
+    //     "function balanceOf(address) public view returns (uint256)",
+    // ];
+    const abiNFT = [
+        "constructor(string memory _name, string memory _symbol, address _signer)",
+        "function name() view returns (string)",
+        "function symbol() view returns (string)",
+        "function mint(address _account, uint256 _tokenId, bytes memory _signature) external",
+        "function ownerOf(uint256) view returns (address)",
+        "function balanceOf(address) view returns (uint256)",
+    ];
+    //直接使用Remix的bytecode，而不是编译后文件中的object对象，那个字节码有问题
+    const bytecodeNFT = contractJson.default.object;
+    const factoryNFT = new ethers.ContractFactory(abiNFT, bytecodeNFT, wallet);
+
+    const balanceETH = await provider.getBalance(wallet);
+    console.log(`钱包ETH余额: ${ethers.formatEther(balanceETH)}`);
+    if (ethers.formatEther(balanceETH) > 0.002) {
+        console.log("开始部署合约")
+        const contractNFT = await factoryNFT.deploy(wallet.address);
+        console.log(`合约地址${contractNFT.target}`);
+        console.log("等待合约部署上链");
+        await contractNFT.waitForDeployment();
+        console.log("合约已上链");
+        console.log(`代币名称: ${await contractNFT.name()}`);
+        console.log(`代币简称: ${await contractNFT.symbol()}`);
+/**
+        console.log("\n4.调用mint函数, 利用签名验证白名单,给account账户铸造NFT");
+        let tx = await contractNFT.mint(account, tokenId, signature);
+        console.log("铸造中,等待交易上链")
+        await tx.wait();
+        console.log(`铸造成功，地址${account}的NFT余额为${await contractNFT.balanceOf(account)}`)
+*/
+    } 
+}
+main()
---
+++ 19_Signature.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+// ECDSA库
+library ECDSA{
+    /**
+     * @dev 通过ECDSA，验证签名地址是否正确，如果正确则返回true
+     * _msgHash为消息的hash
+     * _signature为签名
+     * _signer为签名地址
+     */
+    function verify(bytes32 _msgHash, bytes memory _signature, address _signer) internal pure returns (bool) {
+        return recoverSigner(_msgHash, _signature) == _signer;
+    }
+
+    // @dev 从_msgHash和签名_signature中恢复signer地址
+    function recoverSigner(bytes32 _msgHash, bytes memory _signature) internal pure returns (address){
+        // 检查签名长度，65是标准r,s,v签名的长度
+        require(_signature.length == 65, "invalid signature length");
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // 目前只能用assembly (内联汇编)来从签名中获得r,s,v的值
+        assembly {
+            /*
+            前32 bytes存储签名的长度 (动态数组存储规则)
+            add(sig, 32) = sig的指针 + 32字节
+            等效为略过signature的前32 bytes，即忽略第1个槽位，从下个槽位的起点开始
+            mload(p) 载入从内存地址p起始的接下来32 bytes数据
+            */
+            // 读取长度数据后的32 bytes
+            r := mload(add(_signature, 0x20))
+            // 读取之后的32 bytes
+            s := mload(add(_signature, 0x40))
+            // 读取最后一个byte todo syj 待确认
+            v := byte(0, mload(add(_signature, 0x60)))
+        }
+        // 使用ecrecover(全局函数)：利用 msgHash 和 r,s,v 恢复 signer 地址
+        return ecrecover(_msgHash, v, r, s);
+    }
+    
+    /**
+     * @dev 返回 以太坊签名消息
+     * `hash`：消息哈希 
+     * 遵从以太坊签名标准：https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
+     * 以及`EIP191`:https://eips.ethereum.org/EIPS/eip-191`
+     * 添加"\x19Ethereum Signed Message:\n32"字段，防止签名的是可执行交易。
+     */
+    function toEthSignedMessageHash(bytes32 hash) public pure returns (bytes32) {
+        // 32 is the length in bytes of hash,
+        // enforced by the type signature above
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+}
+
+contract SYJSignature is ERC721, Ownable {
+
+    //项目方公钥
+    address immutable public signer; 
+    mapping(address => bool) public mintedAddress;   // 记录已经mint的地址
+
+    constructor(address _signer) ERC721("SYJMERKLE", "SYJ")
+    {
+        signer = _signer;
+    }
+
+    // 利用ECDSA验证签名并mint
+    function mint(address _account, uint256 _tokenId, bytes memory _signature)
+    external
+    {
+         // 将_account和_tokenId打包消息
+        bytes32 _msgHash = getMessageHash(_account, _tokenId);
+        // 计算以太坊签名消息，该方法是个公用的，所以提取到library中
+        bytes32 _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(_msgHash); 
+        // ECDSA检验通过
+        require(verifySig(_ethSignedMessageHash, _signature), "Invalid signature"); 
+        require(!mintedAddress[_account], "Already minted!"); // 地址没有mint过
+                
+        mintedAddress[_account] = true; // 记录mint过的地址
+        _mint(_account, _tokenId); // mint
+    }
+
+    function getMessageHash(address account, uint256 tokenId) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(account, tokenId));
+    }
+
+    //msgHash消息哈希，signature消息签名, singer消息公钥
+    function verifySig(bytes32 _ethSignedMessageHash, bytes memory _signature) internal view returns(bool) {
+        return ECDSA.verify(_ethSignedMessageHash, _signature, signer);
+    }
+}
